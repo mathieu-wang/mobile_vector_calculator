9c8ed87ae40859e3d91cb0293ae94c558aa9976d
Mathieu Wang <mathieu.sy.wang@gmail.com>
2015-02-23 01:40:31 -0500

    Make scalar product work

--- app/src/main/java/com/sep/vectorcalculator/ScalarProductActivity.java
+++ app/src/main/java/com/sep/vectorcalculator/ScalarProductActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
@@ -81,7 +82,41 @@ public class ScalarProductActivity extends ActionBarActivity {
             }
         }
     }
-    
+
+    public void scalarCalculate(View view) {
+        EditText editTextX1 = (EditText)findViewById(R.id.scalar_editTextX1);
+        EditText editTextY1 = (EditText)findViewById(R.id.scalar_editTextY1);
+        EditText editTextX2 = (EditText)findViewById(R.id.scalar_editTextX2);
+        EditText editTextY2 = (EditText)findViewById(R.id.scalar_editTextY2);
+
+        TextView textViewResult = (TextView)findViewById(R.id.scalarViewResult);
+        String displayText = "";
+
+        try {
+            double x1 = Double.parseDouble(editTextX1.getText().toString());
+            double y1 = Double.parseDouble(editTextY1.getText().toString());
+            double x2 = Double.parseDouble(editTextX2.getText().toString());
+            double y2 = Double.parseDouble(editTextY2.getText().toString());
+
+            if (isCartesian) {
+                CartesianVector cVector1 = new CartesianVector(x1, y1);
+                CartesianVector cVector2 = new CartesianVector(x2, y2);
+                double result = Calculator.cartesianVectorsScalarProduct(cVector1, cVector2);
+                displayText = String.valueOf(result);
+            } else {
+                PolarVector cVector1 = new PolarVector(x1, y1);
+                PolarVector cVector2 = new PolarVector(x2, y2);
+                double result = Calculator.polarVectorsScalarProduct(cVector1, cVector2);
+                displayText = String.valueOf(result);
+            }
+        } catch (NumberFormatException nfe) {
+            displayText = "Please do not leave vector components empty";
+        } catch (Exception e) {
+            displayText = e.getMessage();
+        }
+        textViewResult.setText(displayText);
+    }
+
 
     /**
      * A placeholder fragment containing a simple view.
